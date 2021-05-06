3318d6a1635b964fcd604b1bd98bbacf4dcb9928
Michelangelo <michelangelo@Michelangelo.local>
2021-05-06 12:39:16 +0200

    webcv18commit

--- WEBCV/wsgi.py
+++ WEBCV/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WEBCV.settings')
 
 application = get_wsgi_application()
-
-application = Cling(get_wsgi_application())
