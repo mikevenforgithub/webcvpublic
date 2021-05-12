67d6bcac38b0eb4f99c29cdf7b41c096f185144d
Michelangelo <michelangelo@Michelangelo.local>
2021-05-13 01:16:49 +0200

    finalversion1

--- WEBCV/settings.py
+++ WEBCV/settings.py
@@ -122,37 +122,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+#MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'CV/static/images')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'CV/static/images')
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 
 
 #S3 BUCKETS CONFIG
 
-#AWS_ACCESS_KEY_ID = 'AKIAWEGDNECSJFYFIVTH'
-#AWS_SECRET_ACCESS_KEY = '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'
-#AWS_STORAGE_BUCKET_NAME = 'my-webcv-mv-bucket'
-#AWS_S3_FILE_OVERWRITE = False
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#AWS_S3_OBJECT_PARAMETERS = {'CacheControl' : 'max-age=86400'}
-#AWS_DEFAULT_ACL = 'public-read'
+AWS_ACCESS_KEY_ID = 'AKIAWEGDNECSJFYFIVTH'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'my-webcv-mv-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl' : 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
 
-#AWS_LOCATION = 'static'
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR,'static'),
-#]
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATIC_URL= 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL= 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
-#MEDIA_URL = '/images/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'CV/static/images')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'CV/static/images')
